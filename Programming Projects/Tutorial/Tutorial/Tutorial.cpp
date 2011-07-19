6172748d234cac9c7ff6186c209cbc81fab338be
Kevin Lam <simiopolis@gmail.com>
2011-07-19 18:56:08 -0500

    Cleaned up a bit more by removing folders that were not meant to be uploaded.

--- Programming Projects/Tutorial/Tutorial/Tutorial.cpp
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-	 
-	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	        MessageBox(NULL, "Hello World" , "Tutorial", 0);
-	        return 0;
-	}
