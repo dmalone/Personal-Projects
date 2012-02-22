77866836ed03e42c1b5168fa6aab7f9bb0bfabdb
Thinh Lam <thinh.lam@utexas.edu>
2012-02-21 22:53:40 -0600

    Made more changes.

--- Programming Projects/JavaProjects/ProblemSet2/src/pset2/MyString.java
+++ Programming Projects/JavaProjects/ProblemSet2/src/pset2/MyString.java
@@ -142,7 +142,7 @@ public class MyString {
 			offset++;
 		}
 		return;
-		// System.out.print(contents);
+		
 
 	}
 
--- Programming Projects/JavaProjects/ProblemSet2/src/pset2/Tester.java
+++ Programming Projects/JavaProjects/ProblemSet2/src/pset2/Tester.java
@@ -5,7 +5,7 @@ public class Tester {
 	public static void main(String args[]) {
 		MyString mystring = new MyString("1123");
 		//mystring.insert(5, 21324543);
-		System.out.println(mystring.intValue());
+		//System.out.println(mystring.intValue());
 		System.out.println(mystring);
 	}
 }
